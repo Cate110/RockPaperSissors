16b9e7267642372dca9f7c47f8829f38bae256f9
Julian <julycate11@gmail.com>
2020-11-10 04:12:37 -0300

    I almost finish the game script

--- myscript.js
+++ myscript.js
@@ -1,5 +1,65 @@
-const options = ['rock', 'paper', 'sissors'];
+let playerScore = 0;
+let computerScore = 0;
 
+// Game Logic
 function computerPlay (){
-    return options[~~(Math.random() * options.length)];
+    function random(number) {
+        return Math.floor(Math.random() * number + 1);
+      }
+
+    let computerNumber = random(3);
+    let computerGuess = '';
+  
+    switch (computerNumber) {
+      case 1:
+        computerGuess = 'Rock';
+        break;
+      case 2:
+        computerGuess = 'Paper';
+        break;
+      case 3:
+        computerGuess = 'Scissors';
+        break;
+      default:
+        break;
+    }
+  
+    return computerGuess;
 }
+
+function playRound (playerSelection, computerSelection){
+    playerSelection.toLowerCase();
+    computerSelection.toLowerCase();
+    let log = '';
+
+    if (playerSelection === computerSelection){
+        return log = 'Is a tie!';
+
+    }else if (playerSelection === 'rock' && computerSelection === 'sissors' ||
+              playerSelection === 'sissors' && computerSelection === 'paper' ||
+              playerSelection === 'paper' && computerSelection === 'rock') {
+                 return log = 'You Win!';
+
+    }else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+              playerSelection === 'sissors' && computerSelection === 'rock' ||
+              playerSelection === 'paper' && computerSelection === 'sissors') {
+                 return log = 'You Lose!';
+
+    }else {
+        return log = 'What?';
+    }
+
+    return log; 
+}
+
+let roundResult = playRound (playerSelection, computerSelection);
+
+if (roundResult.search('You Win!') > -1) {
+  playerScore++;
+} else if (roundResult.search('You Lose!') > -1) {
+  computerScore++;
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
